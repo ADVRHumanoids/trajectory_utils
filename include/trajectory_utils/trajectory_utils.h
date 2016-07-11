4d1ab8d77bd4b1122efe06d8ac0e249781d22d40
Enrico Mingo <enrico.mingo@iit.it>
2016-07-11 15:47:26 +0200

    First refactor

---
+++ include/trajectory_utils/trajectory_utils.h
@@ -0,0 +1,231 @@
+/*
+ * Copyright (C) 2016 Walkman
+ * Author: Enrico Mingo Hoffman
+ * email:  enrico.mingo@iit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include <kdl/path_circle.hpp>
+#include <kdl/path_line.hpp>
+#include <kdl/path_point.hpp>
+#include <kdl/path_composite.hpp>
+
+#include <kdl/trajectory_composite.hpp>
+#include <kdl/trajectory_segment.hpp>
+#include <kdl/trajectory_stationary.hpp>
+
+#include <kdl/velocityprofile_trap.hpp>
+#include <kdl/velocityprofile_spline.hpp>
+
+#include <kdl/rotational_interpolation_sa.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+namespace trajectory_utils{
+
+enum velocity_profile{
+    BANG_COAST_BANG
+};
+
+class trajectory_generator{
+public:
+    /**
+     * @brief trajectory_generator constructor
+     * @param dt loop time
+     */
+    trajectory_generator(const double dt);
+
+    /**
+     * @brief getTrajectory
+     * @return the sharep pointer to the KDL trajectory object
+     */
+    const boost::shared_ptr<KDL::Trajectory_Composite>& getTrajectory();
+
+    /**
+     * @brief resetTrajectory clear the trajectory object (all the stored trajectories are deleted)
+     */
+    void resetTrajectory();
+
+    /**
+     * @brief addLineTrj add a set of linear trajectories, specified by way-points and each
+     * costituted by a BANG_COAST_BANG velocity profile. Here we assume that the BANG phases least as
+     * the COAST phase
+     * @param way_points NOTE that the first way-point is the start!
+     * @param T all the segments least the same time
+     * @return true if the trajectories has been added
+     */
+    bool addLineTrj(const std::vector<KDL::Frame>& way_points, double T);
+
+    /**
+     * @brief addLineTrj add a set of linear trajectories, specified by way-points and each
+     * costituted by a BANG_COAST_BANG velocity profile. Here we assume that the BANG phases least as
+     * the COAST phase
+     * @param way_points NOTE that the first way-point is the start!
+     * @param T duration of each segment of trajectory
+     * @return true if the trajectories has been added
+     */
+    bool addLineTrj(const std::vector<KDL::Frame>& way_points, const std::vector<double> T);
+
+    /**
+     * @brief addLineTrj add a set of linear trajectories, specified by way-points,
+     * each constituted by a linear path and a specified velocity profile.
+     * @param vel_profile desired velocity profile
+     * @param way_points NOTE that the first way-point is the start!
+     * @param max_vels max velocity for all the trajectories
+     * @param max_accs max acceleration for all the trajectories
+     * @return true if the trajectories has been added
+     */
+    bool addLineTrj(const velocity_profile vel_profile, const std::vector<KDL::Frame>& way_points,
+                     const double max_vel, const double max_acc);
+
+    /**
+     * @brief addLineTrj add a set of linear trajectories, specified by way-points,
+     * each constituted by a linear path and a specified velocity profile.
+     * @param vel_profile desired velocity profile
+     * @param way_points NOTE that the first way-point is the start!
+     * @param max_vels max velocity for each trajectory
+     * @param max_accs max acceleration for each trajectory
+     * @return true if the trajectories has been added
+     */
+    bool addLineTrj(const velocity_profile vel_profile, const std::vector<KDL::Frame>& way_points,
+                     const std::vector<double> max_vels, const std::vector<double> max_accs);
+    /**
+     * @brief addLineTrj add a trajectory constituted by a linear path and a specified velocity profile
+     * @param vel_profile desired velocity profile
+     * @param start Frame
+     * @param end Frame
+     * @param max_vel max velocity of the trajectory
+     * @param max_acc max acceleration of the trajectory
+     * @return true if the trajectory has been added
+     */
+    bool addLineTrj(const velocity_profile vel_profile, const KDL::Frame& start, const KDL::Frame& end,
+                    const double max_vel, const double max_acc);
+
+    /**
+     * @brief addLineTrj add a trajectory of duration T constituted by a linear path and a BANG_COAST_BANG
+     * velocity profile. Here we assume that the BANG phases least as the COAST phase
+     * @param start Frame
+     * @param end Frame
+     * @param T total time of the trajectory
+     * @return true if the trajectory has been added
+     */
+    bool addLineTrj(const KDL::Frame& start, const KDL::Frame& end, const double T);
+
+    /**
+     * @brief Pos return a frame from the trajectory at time t
+     * @param t time
+     * @return a frame
+     */
+    KDL::Frame Pos(double t);
+
+    /**
+     * @brief Vel a twist from the trajectory at time t
+     * @param t time
+     * @return a twist
+     */
+    KDL::Twist Vel(double t);
+
+    /**
+     * @brief Acc a twist (representing accelerations) from the trajectory at time t
+     * @param t time
+     * @return a twist
+     */
+    KDL::Twist Acc(double t);
+
+    /**
+     * @brief Pos a frame from the trajectory at actual (internal) time
+     * @return a frame
+     */
+    KDL::Frame Pos();
+
+    /**
+     * @brief Vel a twist from the trajectory at actual (internal) time
+     * @return a twist
+     */
+    KDL::Twist Vel();
+
+    /**
+     * @brief Acc a twist (representing accelerations) from the trajectory at actual (internal) time
+     * @return a twist
+     */
+    KDL::Twist Acc();
+
+    /**
+     * @brief updateTrj increment internal time
+     */
+    void updateTrj();
+
+    /**
+     * @brief resetInternalTime set internal time to 0
+     */
+    void resetInternalTime();
+
+    /**
+     * @brief getTime return internal time
+     * @return time
+     */
+    double getTime();
+
+    /**
+     * @brief Duration fot he whole trajectory
+     * @return time of the trajectory
+     */
+    double Duration();
+
+    /**
+     * @brief isFinished
+     * @return true if internal time >= duration
+     */
+    bool isFinished();
+
+    /**
+     * @brief isStarted
+     * @return true if time > 0.0
+     */
+    bool isStarted();
+
+    /**
+     * @brief isRunning
+     * @return true if started and not finished
+     */
+    bool isRunning();
+
+    /**
+     * @brief isInited
+     * @return true if at least one trajectory has been added
+     */
+    bool isInited();
+
+private:
+    double _dt;
+    double _time;
+    double _eq_radius;
+    bool   _is_inited;
+
+    boost::shared_ptr<KDL::Trajectory_Composite> _trj;
+
+    boost::shared_ptr<KDL::Path> createLinePath(const KDL::Frame& start, const KDL::Frame& end);
+
+    boost::shared_ptr<KDL::VelocityProfile> createTrapezoidalVelProfile(const double max_vel, const double max_acc,
+                                                                        const double L);
+
+   bool checkIfCoastPhaseExists(const double max_vel, const double max_acc, const double L);
+
+   void normalizeQuaternion(KDL::Frame& T);
+
+   void computeMaxVelAndMaxAccForBCB(const double T, const double L, double& max_vel, double& max_acc);
+};
+
+}
---
+++ src/trajectory_utils.cpp
@@ -0,0 +1,209 @@
+#include <trajectory_utils/trajectory_utils.h>
+#include <idynutils/cartesian_utils.h>
+
+using namespace trajectory_utils;
+
+trajectory_generator::trajectory_generator(const double dt):
+    _dt(dt),
+    _time(0.0),
+    _eq_radius(0.01),
+    _is_inited(false)
+{
+    _trj.reset(new KDL::Trajectory_Composite());
+}
+
+void trajectory_generator::resetTrajectory()
+{
+    _time = 0.0;
+    _is_inited = false;
+    _trj.reset(new KDL::Trajectory_Composite());
+}
+
+bool trajectory_generator::addLineTrj(const std::vector<KDL::Frame>& way_points, double T)
+{
+    std::vector<double> Ts; Ts.reserve(way_points.size()-1);
+    for(unsigned int i = 0; i < way_points.size()-1; ++i)
+        Ts.push_back(T);
+    return addLineTrj(way_points, Ts);
+}
+
+bool trajectory_generator::addLineTrj(const std::vector<KDL::Frame>& way_points, const std::vector<double> T)
+{
+    bool a = true;
+    for(unsigned int i = 0; i < way_points.size()-1; ++i)
+        a = a && addLineTrj(way_points[i], way_points[i+1], T[i]);
+    _is_inited = a;
+    return _is_inited;
+}
+
+bool trajectory_generator::addLineTrj(const velocity_profile vel_profile, const std::vector<KDL::Frame>& way_points,
+                 const double max_vel, const double max_acc)
+{
+    std::vector<double> max_vels; max_vels.reserve(way_points.size()-1);
+    std::vector<double> max_accs; max_accs.reserve(way_points.size()-1);
+    for(unsigned int i = 0; i < way_points.size()-1; ++i){
+        max_vels.push_back(max_vel);
+        max_accs.push_back(max_acc);}
+    return addLineTrj(vel_profile, way_points, max_vels, max_accs);
+}
+
+bool trajectory_generator::addLineTrj(const velocity_profile vel_profile, const std::vector<KDL::Frame>& way_points,
+                 const std::vector<double> max_vels, const std::vector<double> max_accs)
+{
+    bool a = true;
+    for(unsigned int i = 0; i < way_points.size()-1; ++i)
+        a = a && addLineTrj(vel_profile, way_points[i], way_points[i+1], max_vels[i], max_accs[i]);
+    _is_inited = a;
+    return _is_inited;
+}
+
+bool trajectory_generator::addLineTrj(const KDL::Frame& start, const KDL::Frame& end, const double T)
+{
+    KDL::Frame _start = start; normalizeQuaternion(_start);
+    KDL::Frame _end = end; normalizeQuaternion(_end);
+
+    boost::shared_ptr<KDL::Path> _tmp_path = createLinePath(start, end);
+    double max_vel = 0.0;
+    double max_acc = 0.0;
+
+    computeMaxVelAndMaxAccForBCB(T, _tmp_path->PathLength(), max_vel, max_acc);
+
+    return addLineTrj(BANG_COAST_BANG, start, end, max_vel, max_acc);
+}
+
+bool trajectory_generator::addLineTrj(const velocity_profile vel_profile, const KDL::Frame& start, const KDL::Frame& end,
+                                      const double max_vel, const double max_acc)
+{
+    boost::shared_ptr<KDL::Path> _path = createLinePath(start, end);
+
+    boost::shared_ptr<KDL::VelocityProfile> _velocity_profile;
+    switch (vel_profile) {
+    case BANG_COAST_BANG:
+        if(checkIfCoastPhaseExists(max_vel, max_acc, _path->PathLength()))
+            return false;
+        _velocity_profile = createTrapezoidalVelProfile(max_vel, max_acc, _path->PathLength());
+        break;
+    default:
+        break;
+    }
+
+    boost::shared_ptr<KDL::Trajectory_Segment> _trj_segment;
+    _trj_segment.reset(new KDL::Trajectory_Segment(_path.get()->Clone(),
+                                                   _velocity_profile.get()->Clone()));
+
+    _trj->Add(_trj_segment.get()->Clone());
+
+    _is_inited = true;
+    _time = 0.0;
+
+    return _is_inited;
+}
+
+const boost::shared_ptr<KDL::Trajectory_Composite>& trajectory_generator::getTrajectory()
+{
+    return _trj;
+}
+
+boost::shared_ptr<KDL::Path> trajectory_generator::createLinePath(const KDL::Frame& start, const KDL::Frame& end)
+{
+    KDL::Frame _start = start; normalizeQuaternion(_start);
+    KDL::Frame _end = start; normalizeQuaternion(_end);
+
+    boost::shared_ptr<KDL::Path_Line> _linear_path;
+    _linear_path.reset(new KDL::Path_Line(_start, _end, new KDL::RotationalInterpolation_SingleAxis(), _eq_radius));
+
+    return _linear_path;
+}
+
+boost::shared_ptr<KDL::VelocityProfile> trajectory_generator::createTrapezoidalVelProfile(const double max_vel, const double max_acc,
+                                                                                          const double L)
+{
+    boost::shared_ptr<KDL::VelocityProfile_Trap> _velocity_profile;
+    _velocity_profile.reset(new KDL::VelocityProfile_Trap(max_vel, max_acc));
+    _velocity_profile->SetProfile(0, L);
+
+    return _velocity_profile;
+}
+
+void trajectory_generator::normalizeQuaternion(KDL::Frame& F)
+{
+    double x,y,z,w;
+    F.M.GetQuaternion(x,y,z,w);
+
+    quaternion tmp_q(x,y,z,w);
+    quaternion::normalize(tmp_q);
+
+    F.M = F.M.Quaternion(tmp_q.x, tmp_q.y,tmp_q.z,tmp_q.w);
+}
+
+bool trajectory_generator::checkIfCoastPhaseExists(const double max_vel, const double max_acc,
+                                                   const double L)
+{
+    if(L <= (max_vel*max_vel)/max_acc){
+        std::cout<<"Too fast trajectory, no Coast phase exists!"<<std::endl;
+        return false;
+    }
+    return true;
+}
+
+void trajectory_generator::computeMaxVelAndMaxAccForBCB(const double T, const double L, double &max_vel, double &max_acc)
+{
+    max_vel = (3.*L)/(2.*T);
+    max_acc = (9.*L)/(2.*T*T);
+}
+
+KDL::Frame trajectory_generator::Pos(double t){
+    return _trj->Pos(t);
+}
+
+KDL::Twist trajectory_generator::Vel(double t){
+    return _trj->Vel(t);
+}
+
+KDL::Twist trajectory_generator::Acc(double t){
+    return _trj->Acc(t);
+}
+
+KDL::Frame trajectory_generator::Pos(){
+    return _trj->Pos(_time);
+}
+
+KDL::Twist trajectory_generator::Vel(){
+    return _trj->Vel(_time);
+}
+
+KDL::Twist trajectory_generator::Acc(){
+    return _trj->Acc(_time);
+}
+
+void trajectory_generator::updateTrj(){
+    _time += _dt;
+}
+
+void trajectory_generator::resetInternalTime(){
+    _time = 0.0;
+}
+
+double trajectory_generator::getTime(){
+    return _time;
+}
+
+double trajectory_generator::Duration(){
+    return _trj->Duration();
+}
+
+bool trajectory_generator::isFinished(){
+    return _time >= _trj->Duration();
+}
+
+bool trajectory_generator::isStarted(){
+    return _time > 0.0;
+}
+
+bool trajectory_generator::isRunning(){
+    return isStarted() && !isFinished();
+}
+
+bool trajectory_generator::isInited(){
+    return _is_inited;
+}
